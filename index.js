9c51f8b6563f94766aca7509d206037cdb59709e
Ubuntu <ubuntu@ip-172-31-19-89.us-east-2.compute.internal>
2019-10-08 23:56:03 +0000

    Added logging using morgan, and initialized the /logs route.

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
+const morgan = require('morgan');
+const fs = require('fs');
+var path = require('path')
 
-app.get('/', function (req, res) { 
-	res.send('Hello World');
+var file = fs.createWriteStream(path.join(__dirname, 'log.log'), {flags: 'a'})
+app.use(morgan('tiny', {stream: file}))
+
+app.get('/version', function (req, res) { 
+	res.send('This is version 0 of the HotBurger service');
+});
+
+app.get('/logs', function(req, res) {
+	res.send('This will be the logs')
 });
 
 app.listen(80, function() {
