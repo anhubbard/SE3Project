49ad1e4d19e899d18bfd1ccf94ad9f922bf3501f
Ubuntu <ubuntu@ip-172-31-29-76.us-east-2.compute.internal>
2019-11-08 22:49:27 +0000

    Made changes to ordering service routes

--- OrderingService/index.js
+++ OrderingService/index.js
@@ -4,19 +4,23 @@ const morgan = require('morgan');
 const fs = require('fs');
 var path = require('path')
 
+/*
 var filein = fs.createWriteStream(path.join(__dirname, 'log.log'), {flags: 'a'})
 app.use(morgan('tiny', {stream: filein}))
+*/
 
 app.get('/version', function (req, res) { 
 	res.send('This is version 0 of the HotBurger service');
 });
 
+/*
 app.get('/logs', function (req, res) {
 	var data = fs.readFileSync('log.log', 'utf8');
 
 	console.log(data);
 	res.send(data);
 });
+*/
 
 app.listen(80, function() {
 	console.log("Listening on port 80");
