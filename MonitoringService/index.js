d89591356fdf22823d8e4e12b93666c2782f6a22
Ubuntu <ubuntu@ip-172-31-29-76.us-east-2.compute.internal>
2019-11-09 00:14:51 +0000

    Added skeleton routes to the monitoring service.

--- MonitoringService/index.js
+++ MonitoringService/index.js
@@ -5,6 +5,22 @@ app.get('/gettotal', function (req, res) {
 	return res.status(200).send("Total earnings: ")
 });
 
+app.get('/gettopseller', function (req, res) {
+	return res.status(200).send("Top selling item: ");
+});
+
+app.get('/getrequestcount', function (req, res) {
+	return res.status(200).send("Total number of requests: ");
+});
+
+app.get('/getlastrequeststatus', function (req, res) {
+	return res.status(200).send("Last request status: ");
+});
+
+app.get('/getlastrequesttime', function (req, res) {
+	return res.status(200).send("Time of last request: ");
+});
+
 app.listen(8080, function() {
 	console.log("Listening on port 8080")
 });
