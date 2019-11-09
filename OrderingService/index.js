0ac8beb0e7f1927b6f496ec89b90705875839f41
Ubuntu <ubuntu@ip-172-31-29-76.us-east-2.compute.internal>
2019-11-09 01:18:52 +0000

    Reverted changes that caused the routes to crash

--- OrderingService/index.js
+++ OrderingService/index.js
@@ -5,9 +5,10 @@ const fs = require('fs');
 var path = require('path');
 var date = new Date();
 
-
+/*
 var filein = fs.createWriteStream(path.join(__dirname, '/Test/log.log'), {flags: 'a'})
 app.use(morgan('tiny', {stream: filein}))
+*/
 
 app.get('/version', function (req, res) { 
 	return res.status(200).send('This is version 1 of the HotBurger service');
