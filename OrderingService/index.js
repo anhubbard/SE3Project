7362d074cb6ac232c51295ef4eada129e916a7e9
Ubuntu <ubuntu@ip-172-31-29-76.us-east-2.compute.internal>
2019-11-09 01:03:03 +0000

    Attempting to add in volumes

--- OrderingService/index.js
+++ OrderingService/index.js
@@ -5,10 +5,9 @@ const fs = require('fs');
 var path = require('path');
 var date = new Date();
 
-/*
-var filein = fs.createWriteStream(path.join(__dirname, 'log.log'), {flags: 'a'})
+
+var filein = fs.createWriteStream(path.join(__dirname, '/Test/log.log'), {flags: 'a'})
 app.use(morgan('tiny', {stream: filein}))
-*/
 
 app.get('/version', function (req, res) { 
 	return res.status(200).send('This is version 1 of the HotBurger service');
@@ -19,7 +18,7 @@ app.get('/getmenu', function (req, res) {
 });
 
 app.post('/purchase/:item/:quantity', function (req, res) {
-	return res.status(200).send("Ordered: " + req.params.quantity + " " + req.params.item + "(s). (" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ")" );
+	return res.status(200).send("Ordered: " + req.params.quantity + " " + req.params.item + "(s). (" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + " UTC)" );
 });
 
 app.listen(80, function() {
