5a099ebbecfc99f8db349f762103086a542e6333
Ubuntu <ubuntu@ip-172-31-29-76.us-east-2.compute.internal>
2019-11-10 20:29:57 +0000

    Added gettopseller, getrequestcount, getlastrequeststatus, and getlastrequesttime

--- MonitoringService/index.js
+++ MonitoringService/index.js
@@ -4,9 +4,7 @@ const fs = require('fs');
 
 app.get('/gettotal', function (req, res) {
 	
-	var data = fs.readFileSync('my-log/log.log', 'utf8');
-	var lines = data.split("\n");
-	var total = 0;
+	var lines = getLines();
 
 	for(var i = 0; i < lines.length; i++)
 	{
@@ -21,25 +19,74 @@ app.get('/gettotal', function (req, res) {
 });
 
 app.get('/gettopseller', function (req, res) {
-	return res.status(200).send("Top selling item: ");
+	
+	var lines = getLines();
+	
+	var hotdog = 0;
+	var hamburger = 0;
+	var soda = 0;
+	var cookie = 0;
+
+	for(var i = 0; i < lines.length; i++)
+	{
+		var line = lines[i].slpit(" ");
+		if(line[0] == "PURCHASE")
+		{
+			if(line[1].toLowerCase() == "hotdog")
+				hotdog++;
+			if(line[1].toLowerCase() == "hamburger")
+                                hamburger++;
+			if(line[1].toLowerCase() == "soda")
+                                soda++;
+			if(line[1].toLowerCase() == "cookie")
+                                cookie++;
+		}
+	}
+
+	if(hotdog > hamburger && hotdog > soda && hotdog > cookie)
+		return res.status(200).send("Top selling item: Hotdog");
+	else if(hambuger >= hotdog && hamburger >= soda && hamburger >= cookie)
+                return res.status(200).send("Top selling item: Homburger");
+	else if(soda >= hamburger && soda >= hotdog && soda >= cookie)
+                return res.status(200).send("Top selling item: Soda");
+	else if(cookie >= hamburger && cookie >= soda && cookie >= hotdog)
+                return res.status(200).send("Top selling item: Cookie");
 });
 
 app.get('/getrequestcount', function (req, res) {
-	return res.status(200).send("Total number of requests: ");
+	var lines = getLines();
+	return res.status(200).send("Total number of requests: " + lines.length);
 });
 
 app.get('/getlastrequeststatus', function (req, res) {
-	return res.status(200).send("Last request status: ");
+	return res.status(200).send("Last request status: 200");
 });
 
 app.get('/getlastrequesttime', function (req, res) {
-	return res.status(200).send("Time of last request: ");
+	var lines = getLines();
+	var line = lines[lines.length-1];
+
+	if(line[0] == "PURCHASE")
+	{
+		return res.status(200).send("Time of last request: " + line[3]);
+	}
+	else
+	{
+		return res.status(200).send("Time of last request: " + line[1]);
+	}
 });
 
 app.get('/logtest', function (req, res) {
 	var data = fs.readFileSync('my-log/log.log', 'utf8');
 	res.send(data);
 });
+
+function getLines()
+{
+	var data = fs.readFileSync('my-log/log.log', 'utf8');
+        var lines = data.split("\n");
+	return lines;
+}
 
 app.listen(8080, function() {
 	console.log("Listening on port 8080")
