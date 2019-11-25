a9e0be13da100c17c38f1b7cc5bff3f20df208b5
Amber Hubbard <anh49@students.uwf.edu>
2019-11-25 22:05:30 +0000

    added connection to fake inventory

--- OrderingService/index.js
+++ OrderingService/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 const morgan = require('morgan');
+const axiox = require('axios');
 const fs = require('fs');
 var path = require('path');
 var date = new Date();
@@ -22,7 +23,17 @@ app.get('/getmenu', function (req, res) {
 });
 
 app.post('/purchase/:item/:quantity', function (req, res) {
-	fs.appendFile('my-log/log.log', ('PURCHASE ' + req.params.item + ' ' + getPrice(req.params.item, req.params.quantity) + getTime() + '\n'), function (err) {});	
+	axios.post('http://fake_inventory_service:8082/getcount/' + req.params.item, {})
+	.then(function(response) {
+		if(response >= req.params.quantity)
+		{
+			fs.appendFile('my-log/log.log', ('PURCHASE ' + req.params.item + ' ' + getPrice(req.params.item, req.params.quantity) + getTime() + '\n'), function (err) {});
+		}
+		else
+		{
+			fs.appendFile('my-log/log.log', ('PURCHASE ' + req.params.item + ' ' + getPrice(req.params.item, 0) + getTime() + '\n'), function (err) {});
+		}
+	}
 	return res.status(200).send("Purchase successful!");
 });
 
